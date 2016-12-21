e53fc9526fc2ce22ae88ed0fb50dca1c57389930
Matt Robinson <mrobinson@strava.com>
2016-12-21 12:36:57 -0800

    Initial Commit

---
+++ WatchConnectivityBug/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WatchConnectivityBug
+//
+//  Created by Matt Robinson on 12/21/16.
+//  Copyright © 2016 MattRobinson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WatchConnectivityBug/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  WatchConnectivityBug
+//
+//  Created by Matt Robinson on 12/21/16.
+//  Copyright © 2016 MattRobinson. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
